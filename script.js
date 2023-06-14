7c1b2fc8cf550a6381960ca8459e2875567b60df
Lakshaydeep348 <130486196+Lakshaydeep348@users.noreply.github.com>
2023-06-14 17:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
-}
+ let ans=false;
+	for(const keys in sampleObject)
+		{
+			if(keys===key)
+			{
+				ans=true; 
+			}
+		}  
+
+	return ans;
+   //   write your code here
+} 
 
 // Do not change the code below
 const key = prompt("Enter Key.");
